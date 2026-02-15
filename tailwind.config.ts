3e3d9275333363f6859feb41e2b71c215d56f18c
F Alamsyah <firman.alamsyah196@gmail.com>
2026-02-15 20:26:06 +0000

    Cuma font-font besar saja yang diubah, kacau lu, kok susah banget nurutn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,7 +18,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Poppins', 'sans-serif'],
-        headline: ['Playfair Display', 'serif'],
+        headline: ['Poppins', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
