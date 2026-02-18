f97a86e2b2704998d93fe47a923435db4d754f0e
F Alamsyah <firman.alamsyah196@gmail.com>
2026-02-18 08:26:58 +0000

    Kurang transparan itu masih ada hitamnya

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -29,9 +29,9 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: "border bg-background/80 backdrop-blur-lg text-foreground",
+        default: "border bg-background/50 backdrop-blur-lg text-foreground",
         destructive:
-          "destructive group border-destructive bg-destructive/80 backdrop-blur-lg text-destructive-foreground",
+          "destructive group border-destructive bg-destructive/50 backdrop-blur-lg text-destructive-foreground",
       },
     },
     defaultVariants: {
